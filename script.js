d13eead66e47229b33910cc3165c7802c403b81f
ayoub <ayoubbazaz02@gmail.com>
2025-01-05 21:22:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+ function sendEmail(){
+   var params = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      subject : document.getElementById("subject").value,
+      message : document.getElementById("message").value
+   }
+   emailjs.send("service_nusxcjt","template_l1g1qsh", params).then(function (res){
+    alert("votre message envoyé");
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+
+   })
+ }
